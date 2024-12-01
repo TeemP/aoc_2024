26e2ff1e3d09077105265639723ef8505557c08b
Teemu Pekkanen <teemu.pekkanen@productivityleap.com>
2024-12-01 23:13:53 +0200

    day 1 t 2
    day 1 slices

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -22,18 +22,32 @@ fn read_file(path: &str) -> (Vec<i32>, Vec<i32>) {
         .unzip()
 }
 
-fn calculate_vector_distance(mut vec_a: Vec<i32>, mut vec_b: Vec<i32>) -> i32 {
+fn calculate_vector_distance_by_abs(vec_a: &mut [i32], vec_b: &mut [i32]) -> i32 {
     vec_a.sort();
     vec_b.sort();
-    vec_a.into_iter()
+    vec_a.iter_mut()
         .zip(vec_b)
-        .map(|(a, b)| (a-b).abs())
+        .map(|(a, b)| (*a-*b).abs())
+        .sum::<i32>()
+}
+
+fn calculate_vector_distance_by_count(vec_a: &mut [i32], vec_b: &mut [i32]) -> i32 {
+    vec_a.sort();
+    vec_b.sort();
+    vec_a.iter_mut()
+        .map(|a| {
+            *a*(vec_b.iter_mut()
+            .filter(|v| **v==*a)
+            .count() as i32)
+        })
         .sum::<i32>()
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (location_list_a, location_list_b) = read_file(&args[1]);
-    let distance = calculate_vector_distance(location_list_a, location_list_b);
-    println!("Distance of the lists: {}", distance);
+    let (mut location_list_a, mut location_list_b) = read_file(&args[1]);
+    let distance = calculate_vector_distance_by_abs(&mut location_list_a, &mut location_list_b);
+    println!("Distance of the lists part 1: {}", distance);
+    let distance = calculate_vector_distance_by_count(&mut location_list_a, &mut location_list_b);
+    println!("Distance of the lists part 2: {:?}", distance);
 }
