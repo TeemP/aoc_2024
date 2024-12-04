c8ce5bcdfa8b8fef31b238a9b085123a822d35c6
Teemu Pekkanen <teemu.pekkanen@productivityleap.com>
2024-12-04 21:42:51 +0200

    day 4 t 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -82,6 +82,33 @@ fn find_diagonal_up(needle: &str, input: &[Vec<char>]) -> usize {
     count
 }
 
+fn find_x_sam(input: &[Vec<char>]) -> usize {
+    let needle = "MAS";
+    let needle_len = needle.len();
+    let reverse_needle = &needle.chars().rev().collect::<String>();
+    let y_len = input.len() - needle_len;
+    let x_len = input[0].len() - needle_len;
+    let mut count = 0;
+    for x in 0..x_len + 1{
+        for y in 0..y_len + 1 {
+            let hay_string_1 = &input[y..(y+needle_len)].iter()
+                .enumerate()
+                .map(|(i,row)| row[x+i])
+                .collect::<String>();
+            let hay_string_2 = &input[y..(y+needle_len)].iter()
+                .enumerate()
+                .map(|(i,row)| row[x+needle_len-i-1])
+                .collect::<String>();
+            let hay_string_1_found = (hay_string_1 == needle) | (hay_string_1 == reverse_needle);
+            let hay_string_2_found = (hay_string_2 == needle) | (hay_string_2 == reverse_needle);
+            if hay_string_1_found & hay_string_2_found {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
 fn find_all_words_from_input(needle: &str, input: &[Vec<char>]) -> usize {
     let mut sum = find_horizontal(needle, input);
     sum += find_vertical(needle, input);
@@ -98,6 +125,7 @@ fn main() {
         &needle,
         find_all_words_from_input(needle, &input)
     );
+    println!("X-MAS was found {} times",find_x_sam(&input));
 }
 
 
@@ -126,7 +154,7 @@ mod tests {
         let input = read_file("test_data.txt");
         let needle = "XMAS";
         let result = find_diagonal_down(needle, &input);
-        assert_eq!(result, 2);
+        assert_eq!(result, 5);
     }
 
     #[test]
@@ -137,4 +165,10 @@ mod tests {
         assert_eq!(result, 5);
     }
 
+    #[test]
+    fn test_find_x_sam() {
+        let input = read_file("test_data.txt");
+        let result = find_x_sam(&input);
+        assert_eq!(result, 9);
+    }
 }
